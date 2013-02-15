fbafad13b8f76e53f420461eec8fa7a1457a18a3
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-02-15 18:22:49 +0100

    #2: Design front page and public subpages

--- server/src/main/java/eazyit/Start.java
+++ server/src/main/java/eazyit/Start.java
@@ -1,19 +1,17 @@
-package dk.eazyit;
+package eazyit;
 
-import org.apache.wicket.util.time.Duration;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.bio.SocketConnector;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Start {
     public static void main(String[] args) throws Exception {
-        int timeout = (int) Duration.ONE_HOUR.getMilliseconds();
+
 
         Server server = new Server();
         SocketConnector connector = new SocketConnector();
 
         // Set some timeout options to make debugging easier.
-        connector.setMaxIdleTime(timeout);
         connector.setSoLingerTime(-1);
         connector.setPort(8080);
         server.addConnector(connector);
@@ -21,13 +19,9 @@ public class Start {
         WebAppContext bb = new WebAppContext();
         bb.setServer(server);
         bb.setContextPath("/");
-        bb.setWar("src/main/webapp");
+//        bb.setWar("/mainwar/target/mainwar-1.0-SNAPSHOT.war");
+        bb.setWar("mainwar/target/mainwar-1.0-SNAPSHOT.war");
 
-        // START JMX SERVER
-        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
-        // server.getContainer().addEventListener(mBeanContainer);
-        // mBeanContainer.start();
 
         server.setHandler(bb);
 
