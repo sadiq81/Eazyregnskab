3dc80bf2b5dfdc329e16fbc6a6aeb9998dbb6f72
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-04-26 13:15:10 +0200

    #55: On signup add check to see if username exist

--- mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/validators/CreateAccountFormValidator.java
+++ mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/validators/CreateAccountFormValidator.java
@@ -1,10 +1,12 @@
 package dk.eazyit.eazyregnskab.web.components.validators;
 
+import dk.eazyit.eazyregnskab.services.LoginService;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.FormComponent;
 import org.apache.wicket.markup.html.form.PasswordTextField;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.form.validation.AbstractFormValidator;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.apache.wicket.util.lang.Objects;
 
 import java.util.regex.Pattern;
@@ -16,8 +18,9 @@ public class CreateAccountFormValidator extends AbstractFormValidator {
 
     private static final String USERNAME_PATTERN = "^[a-z0-9_-]{8,25}$";
     private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,25})";
-    protected final static int DURATION = 5;
 
+    @SpringBean
+    LoginService loginService;
 
     private static final long serialVersionUID = 1L;
 
@@ -58,6 +61,9 @@ public class CreateAccountFormValidator extends AbstractFormValidator {
         final FormComponent<?> formComponent2 = components[1];
         final FormComponent<?> formComponent3 = components[2];
 
+        if (loginService.findAppUserByUsername(formComponent1.getInput()) != null) {
+            error(formComponent1, "username.all.ready.exists");
+        }
         if (!Pattern.compile(USERNAME_PATTERN).matcher(formComponent1.getInput()).matches()) {
             error(formComponent2, "username.not.adequate");
         }
