fc6a8ee48ea5aa5a4eabf481bb71ced8950b04b5
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-07-27 20:27:52 +0200

    #119: Remember me not working after restart

--- mainwar/src/main/webapp/WEB-INF/classes/META-INF/remember-me-schema.sql
+++ mainwar/src/main/webapp/WEB-INF/classes/META-INF/remember-me-schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE `persistent_logins` (
+
+
+CREATE TABLE IF NOT EXISTS `persistent_logins` (
   `username` varchar(50) NOT NULL,
   `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `series` varchar(64) NOT NULL,
