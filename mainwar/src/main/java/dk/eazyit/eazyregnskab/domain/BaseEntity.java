6cdc5003c2a94dbc2c46f698d9d6a855577f503a
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-08-04 11:45:40 +0200

    #141: Add created and last saved on entities

--- mainwar/src/main/java/dk/eazyit/eazyregnskab/domain/BaseEntity.java
+++ mainwar/src/main/java/dk/eazyit/eazyregnskab/domain/BaseEntity.java
@@ -1,13 +1,21 @@
 package dk.eazyit.eazyregnskab.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 /**
- * @author Trifork
+ * @author Eazy IT
  */
+@MappedSuperclass
+@Inheritance(strategy= InheritanceType.TABLE_PER_CLASS)
 public abstract class BaseEntity  implements EntityWithLongId {
 
+    @Column(nullable = false)
     protected Date created;
+    @Column(nullable = false)
     protected Date lastChanged;
 
     @Override
