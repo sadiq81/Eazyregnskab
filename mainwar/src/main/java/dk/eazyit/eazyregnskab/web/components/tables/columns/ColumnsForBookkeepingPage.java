97282c28bfedaa778e4ef019ea66c67b4deb7412
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-08-11 22:34:36 +0200

    #173: Create tooltip on dailyledger panel postings for finance account name/number

--- mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/tables/columns/ColumnsForBookkeepingPage.java
+++ mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/tables/columns/ColumnsForBookkeepingPage.java
@@ -29,7 +29,7 @@ public class ColumnsForBookkeepingPage extends ArrayList<IColumn<DraftFinancePos
         add(new NumberPropertyColumn<DraftFinancePosting>(new ResourceModel("amount"), "amount", "amount", 2, 2));
         add(new ToolTipPropertyColumn<DraftFinancePosting>(new ResourceModel("financeAccount"), "financeAccount.accountNumber", "financeAccount.accountNumber", "financeAccount.name"));
         add(new PropertyColumn<DraftFinancePosting, String>(new ResourceModel("vatType"), "vatType.name", "vatType.name"));
-        add(new PropertyColumn<DraftFinancePosting, String>(new ResourceModel("finance.account.reverse"), "reverseFinanceAccount.accountNumber", "reverseFinanceAccount.accountNumber"));
+        add(new ToolTipPropertyColumn<DraftFinancePosting>(new ResourceModel("finance.account.reverse"), "reverseFinanceAccount.accountNumber", "reverseFinanceAccount.accountNumber", "reverseFinanceAccount.name"));
         add(new PropertyColumn<DraftFinancePosting, String>(new ResourceModel("reverseVatType"), "reverseVatType.name", "reverseVatType.name"));
 //        add(new CheckboxPropertyColumn<DraftFinancePosting>(new ResourceModel("chose"),"chosen"));
         add(new AbstractColumn<DraftFinancePosting, String>(new ResourceModel("action")) {
