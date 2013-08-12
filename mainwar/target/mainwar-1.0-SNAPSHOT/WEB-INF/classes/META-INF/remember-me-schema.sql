789b417647f14015d2dc81aca3f80e55c0b323dc
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-08-12 16:14:42 +0200

    Move repo

---
+++ mainwar/target/mainwar-1.0-SNAPSHOT/WEB-INF/classes/META-INF/remember-me-schema.sql
@@ -0,0 +1,8 @@
+
+
+CREATE TABLE IF NOT EXISTS `persistent_logins` (
+  `username` varchar(50) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
