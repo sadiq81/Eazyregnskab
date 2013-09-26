b003d6a91a407e6850fd667451c3894241b8e379
Tommy Sadiq Hinrichsen <tommy@eazyit.dk>
2013-09-26 14:12:08 +0200

    #31 All doubles should only be 2 significant digits

--- mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/converter/EazyDoubleConverter.java
+++ mainwar/src/main/java/dk/eazyit/eazyregnskab/web/components/converter/EazyDoubleConverter.java
@@ -1,8 +1,6 @@
 package dk.eazyit.eazyregnskab.web.components.converter;
 
 import org.apache.wicket.util.convert.converter.DoubleConverter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.math.RoundingMode;
 import java.text.NumberFormat;
@@ -13,16 +11,13 @@ import java.util.Locale;
  */
 public class EazyDoubleConverter extends DoubleConverter {
 
-    private static final Logger LOG = LoggerFactory.getLogger(EazyDoubleConverter.class);
-
     @Override
-    public String convertToString(Double aDouble, Locale locale) {
-        LOG.trace("converting " + aDouble + " to string, locale = " + locale);
-        NumberFormat nf = this.getNumberFormat(locale);
+    protected NumberFormat newNumberFormat(final Locale locale) {
+        NumberFormat nf = NumberFormat.getInstance(locale);
         nf.setMinimumFractionDigits(2);
         nf.setMaximumFractionDigits(2);
         nf.setRoundingMode(RoundingMode.HALF_UP);
-        return nf.format(aDouble);
+        return nf;
     }
 
 }
